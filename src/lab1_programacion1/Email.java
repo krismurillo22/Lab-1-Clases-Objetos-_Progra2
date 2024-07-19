8818c4d36f57f804b6150435aaac353837a16ce9
krismurillo22 <kristian_murillo@unitec.edu>
2024-07-19 13:53:58 -0600

    avril

--- src/lab1_programacion1/Email.java
+++ src/lab1_programacion1/Email.java
@@ -37,7 +37,7 @@ public class Email {
     }
     public void print() {
         System.out.println("De: "+guardarEmail+" Asunto: "+asunto+" Contenido "+contenido+"\n "+Leido);
-        
+        gh0l
     }
 
   
